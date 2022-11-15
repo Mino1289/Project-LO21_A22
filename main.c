dfa7604fdfb0e4a1a8f12848eac5f5d779c1252f
Mino1289 <kretzvictor@orange.fr>
2022-11-15 17:51:12 +0100

    main as expected

--- main.c
+++ main.c
@@ -15,41 +15,37 @@ int main(int argc, char* argv[]) {
 	// -Nombre de générations : 20 ≤ nGen ≤ 200
 
 	uint longIndiv = 16;
-	float pCroise = get_float_input("Quelle probabilité de croisement souhaitez-vous ? (0.1-1): ", 0.1, 1);
-	uint taillePop = get_integer_input("Quelle taille de population souhaitez-vous ? (8-256): ", 8, 256);
+	float pCroise = 0.5;
+	uint taillePop = get_integer_input("Quelle taille de population souhaitez-vous ? (8-256): ", 20, 200);
 	float tSelectp = get_float_input("Quel taux de sélection souhaitez-vous ? (0.1-0.9): ", 0.1, 0.9);
-	uint nGen = get_integer_input("Combien de génération souhaitez-vous ? (4-256): ", 4, 256);
+	uint nGen = get_integer_input("Combien de génération souhaitez-vous ? (4-256): ", 20, 200);
 	
-	uint tSelect = tSelectp*taillePop; // on préfère le nombre qu'un pourcentage de la population
-	printf("%d\n", tSelect);
+	uint tSelect = tSelectp*taillePop; 
+	// on préfère le nombre qu'un pourcentage de la population
 
-	
+
+	// Initialiser la Population 
 	Population p = initPopulation(taillePop, longIndiv), tmp;
-	quicksortPopulation(p, A, B, F);
-	printPopulation(p);
-
-	int i = 0;
-	printf("Valeur\t->\tQualité\t\tBits\n");
-	while (i < nGen) {
+	// Répéter nGen fois 
+	// Début 	
+	for (int i = 0; i < nGen; ++i) {
+		// Croiser la Population 
 		tmp = croisementPopulation(p, pCroise);
 		freeIndividu(p.individus);
+		// Trier la Population 
 		// quicksortPopulation(p); // pas besoin de trier, c'est fait dans select
+		// Sélectionner la Population 
 		p = selectPopulation(tmp, tSelect, A, B, F);
 		freeIndividu(tmp.individus);
-		++i;
 	}
-	printPopulation(p);
-	// // print1Indiv(p.individus);
+	// Fin 
+	quicksortPopulation(p, A, B, F); // on trie pour afficher le meilleur individu
+	// Afficher le meilleur Individu de la Population
+	printf("Meilleur individu : ");
+	printf("Valeur\t->\tQualité\t\tBits\n");
+	print1Indiv(p.individus);
 
 	freeIndividu(p.individus);
-	// freeIndividu(tmp.individus);	
-
-	// Population p = initPopulation(20, 8);
-
-	// Population pop = selectPopulation(p, 5);
-
-	// freeIndividu(p.individus);
-	// freeIndividu(pop.individus);
 
 	printf("Fin du programme\n");
 
